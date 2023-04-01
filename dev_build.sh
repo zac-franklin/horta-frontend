25e8081d6654369a82071d1468bbeadf287a58a9
Zac Pearson <zacfpearson@gmail.com>
2023-04-01 17:30:48 -0500

    rename

--- dev_build.sh
+++ dev_build.sh
@@ -1,4 +1,4 @@
-sudo docker build -f docker/Dockerfile.dev -t sentiment-frontend:dev docker
-sudo docker run -it --mount type=bind,source=$(pwd)/code,target=/build sentiment-frontend:dev /bin/bash -c "cargo build --release --target wasm32-unknown-unknown && wasm-bindgen --out-name sentiment-frontend --out-dir pkg --target web target/wasm32-unknown-unknown/release/sentiment_frontend.wasm"
-sudo docker build -f docker/Dockerfile.serve -t sentiment-frontend:serve code/
-sudo docker run --network=host sentiment-frontend:serve
+sudo docker build -f docker/Dockerfile.dev -t horta-frontend:dev docker
+sudo docker run -it --mount type=bind,source=$(pwd)/code,target=/build horta-frontend:dev /bin/bash -c "cargo build --release --target wasm32-unknown-unknown && wasm-bindgen --out-name horta-frontend --out-dir pkg --target web target/wasm32-unknown-unknown/release/horta_frontend.wasm"
+sudo docker build -f docker/Dockerfile.serve -t horta-frontend:serve code/
+sudo docker run --network=host horta-frontend:serve
